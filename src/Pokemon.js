a3a02a1ebe743b9f7225268fd574176bf105b00b
Naomi Ainspan <nainspan@gmail.com>
2023-02-07 20:12:04 -0500

    made async await program

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,17 +1,33 @@
 import axios from 'axios';
 
 const Pokemon = () => {
-    let pokemon = axios
-    .get('https://pokeapi.co/api/v2/pokemon')
-    .then((response) => {
-        console.log(response);
-    });
-    return(
-        
-        {pokemon.data.results.map(pokemon => {
-            return <p>{pokemon.name}</p>
-        })}
-    )
+    ///asynch example with 
+//     try{
+//     let pokemon = axios
+//         .get('https://pokeapi.co/api/v2/pokemon')
+//         .then((response) => {
+//             console.log(response);
+//     });
+// } catch (err){
+//     console.log(err)
+// }
+
+//////asynch await
+const getPokemon = asynch () => {
+    try{
+        const {data} = axios.get('https://pokeapi.co/api/v2/pokemon')
+        console.log(data);
+    } catch (err) {
+        console.log(err);
+    }
 }
 
+    return(
+       <>
+       <p>hi pokemon</p>
+       <button onClick={getPokemon()}>Get Pokemon</button>
+       </>
+    )
+
+}
 export default Pokemon;
